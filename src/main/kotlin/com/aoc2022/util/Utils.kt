954f910f0132260dbdd09782182e47cf3dd855a2
Kate Shaffer <kate.shaffer@gmail.com>
2022-12-17 07:43:22 -0800

    Add Util

---
+++ src/main/kotlin/com/aoc2022/util/Utils.kt
@@ -0,0 +1,16 @@
+package com.aoc2022.util
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class Utils {
+  companion object {
+    fun readFileAsMutableList(relFilePath: String): MutableList<String> {
+      return Files
+        .readAllLines(
+          Paths.get("./src/main/kotlin/com/aoc2022/$relFilePath")
+        )
+        .toMutableList()
+    }
+  }
+}
