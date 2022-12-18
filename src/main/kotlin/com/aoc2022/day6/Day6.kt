bf4e25d80ba2e192a75d1beb420904fbc7e3db30
Kate Shaffer <kate.shaffer@gmail.com>
2022-12-18 00:26:13 -0800

    Day 6, part 2 works with ugly testing

--- src/main/kotlin/com/aoc2022/day6/Day6.kt
+++ src/main/kotlin/com/aoc2022/day6/Day6.kt
@@ -19,17 +19,32 @@ class Day6 {
 //      println("should be 10")
 //      println(findMarkerIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
 //      println("should be 11")
+      println(findMessageIndex(signal))
+      println(findMessageIndex("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+      println("should be 19")
+      println(findMessageIndex("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+      println("should be 23")
+      println(findMessageIndex("nppdvjthqldpwncqszvftbrmjlhg"))
+      println("should be 23")
+      println(findMessageIndex("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+      println("should be 29")
+      println(findMessageIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+      println("should be 26")
+
     }
 
     private fun parseInput(): String {
       return readFileAsMutableList("day6/Input").first()
     }
 
-    private fun findMarkerIndex(signal: String): Int {
+    private fun findIndexForDistinctSize(
+      signal: String,
+      size: Int
+    ): Int {
       var markerIndex = 0
 
-      for (i in 3 until signal.length) {
-        if (signal.slice(i-3..i).toSet().size < 4) {
+      for (i in size-1 until signal.length) {
+        if (signal.slice(i-(size-1)..i).toSet().size < size) {
           continue
         }
         markerIndex = i+1
@@ -37,6 +52,20 @@ class Day6 {
       }
 
       return markerIndex
+    }
+
+    private fun findMarkerIndex(signal: String): Int {
+      return findIndexForDistinctSize(
+        signal = signal,
+        size = 4,
+      )
+    }
+
+    private fun findMessageIndex(signal: String): Int {
+      return findIndexForDistinctSize(
+        signal = signal,
+        size = 14,
+      )
     }
   }
 }
